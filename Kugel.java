f3e039695d790f4e30d496ac048f04235740208b
Simon <S.Ohrendorf@web.de>
2021-06-28 11:23:09 +0200

    add Bubbelsort

---
+++ Kugel.java
@@ -0,0 +1,27 @@
+
+
+public class Kugel
+{
+    private int zahl;
+    private String farbe;
+    private boolean halb; 
+
+    public Kugel(int pZahl, String pFarbe, boolean pHalb)
+    {
+        zahl = pZahl;
+        farbe = pFarbe; 
+        halb = pHalb;
+    }
+    
+    public int getZahl(){
+        return zahl;
+    }
+    
+    public String getFarbe(){
+        return farbe;
+    }
+    
+    public boolean getHalb(){
+        return halb;
+    }
+}
